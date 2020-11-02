975628c804fd772e6a1cf13ec4b1774402717074
Mr.Universe <yurilucaspg@gmail.com>
2020-11-02 01:49:13 -0300

    EX04

--- C/avaliacao_2/ex04/ex04.c
+++ C/avaliacao_2/ex04/ex04.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
-
 typedef struct no{
     int dado;
     struct no* prox;
@@ -21,7 +18,7 @@ No* criar_no(int  elemento){
 
 void listaOcorrencia_toString(No** lista, int numLinhas, char* saida){
     int i, pos = 0;
-    
+
     for(i=0; i<numLinhas; i++){
         saida[pos++] = '[';
         No* aux = lista[i];
@@ -29,7 +26,7 @@ void listaOcorrencia_toString(No** lista, int numLinhas, char* saida){
             char str[10];
             sprintf(str, "%d", aux->dado);
             int numCaracteres = (int)strlen(str);
-            memcpy(saida+pos, str, numCaracteres*sizeof(char));            
+            memcpy(saida+pos, str, numCaracteres*sizeof(char));
             pos += numCaracteres;
 
             if(aux->prox != NULL) saida[pos++] = ',';
@@ -48,25 +45,35 @@ void listaOcorrencia_toString(No** lista, int numLinhas, char* saida){
 * EXERCICIO 04
 **************************************/
 No** listaOcorrencia(int* mat, int linhas, int colunas){
+    No* aux;
     int i,j;
 
     // cria um vetor de ponteiros de nós e inicializa as posicoes com NULL
     No** lista = (No**)calloc(linhas, sizeof(No*));
     for(i=0; i<linhas; i++) lista[i] = NULL;
-    
 
     for(i=0; i<linhas; i++){
         for(j=0; j<colunas; j++){
-            printf("%3d ", *(mat + (i*colunas) + j));
+            if(*((mat + (i*colunas)) + j) != 0){
+                if(lista[i] != NULL){
+                    No* novo = (No*) malloc(sizeof(No*));
+                    aux = lista[i];
+                    novo->dado = *((mat + (i*colunas)) + j);
+                    novo->prox = aux;
 
-            
-            
+                    lista[i] = novo;
+                }else{
+                    lista[i] = (No*) malloc(sizeof(No*));
+                    lista[i]->dado = *((mat + (i*colunas)) + j);
+                    lista[i]->prox = NULL;
+                }
+            }
+            printf("%3d ", *((mat + (i*colunas)) + j));
         }
-
         printf("\n");
     }
     return lista;
-} 
+}
 
 
 int main(){
@@ -75,7 +82,7 @@ int main(){
     printf("\n----------------------------------------------\n");
     printf(">>> TESTE 01                                    \n");
     printf("------------------------------------------------\n");
-    int matriz1[4][3] ={{1,0,2},{0,0,0},{3,4,5}, {0,0,6}};  
+    int matriz1[4][3] ={{1,0,2},{0,0,0},{3,4,5}, {0,0,6}};
 
     No** lista1 = listaOcorrencia(&matriz1[0][0], 4, 3);
     listaOcorrencia_toString(lista1, 4, string);
@@ -84,7 +91,7 @@ int main(){
     printf("\n----------------------------------------------\n");
     printf(">>> TESTE 02                                    \n");
     printf("------------------------------------------------\n");
-    int matriz2[3][3] ={{0,0,0},{0,0,0},{0,50,0}};  
+    int matriz2[3][3] ={{0,0,0},{0,0,0},{0,50,0}};
 
     No** lista2 = listaOcorrencia(&matriz2[0][0], 3, 3);
     listaOcorrencia_toString(lista2, 3, string);
@@ -93,7 +100,7 @@ int main(){
     printf("\n----------------------------------------------\n");
     printf(">>> TESTE 03                                    \n");
     printf("------------------------------------------------\n");
-    int matriz3[3][3] ={{1,2,3},{4,5,6},{7,8,9}};  
+    int matriz3[3][3] ={{1,2,3},{4,5,6},{7,8,9}};
 
     No** lista3 = listaOcorrencia(&matriz3[0][0], 3, 3);
     listaOcorrencia_toString(lista3, 3, string);
